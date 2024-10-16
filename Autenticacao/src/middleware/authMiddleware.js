3b9c21c085d4a73f7c1ca02550665c8005640205
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-10-16 20:31:17 -0300

    Update authMiddleware.js

--- Autenticacao/src/middleware/authMiddleware.js
+++ Autenticacao/src/middleware/authMiddleware.js
@@ -2,7 +2,16 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-
+  try {
+        const tolken = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, "secret")
+        req.userData = decoded; //estou adicionando mais um item na minha requisição
+        next(); //apartir daqui ele continua
+        //Bearer: ele passa coisas aleatórias
+        //depois um array do Bearer com coisas aleatórias com a posição 1
+    } catch (error) {
+        return res.status(401).send({ mensagem: "Falha na autenticação!" })
+    }
 };
 
 module.exports = auth;
